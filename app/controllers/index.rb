f3b4cc10eb4777c4a20fe145ce27cf856c1eb6e4
Absoludacris06 <nicholas.saldivar@gmail.com>
2013-09-10 19:41:00 -0700

    update auth route logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,12 +16,16 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   session.delete(:request_token)
 
-  @user = User.new(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
-  if @user.save
-    session[:user_id] = @user.id
-    erb :index
+  if User.find_by_username(@access_token.params[:screen_name]) == nil
+    @user = User.new(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+    if @user.save
+      session[:user_id] = @user.id
+      erb :index
+    else
+      redirect to('/')
+    end
   else
-    redirect to('/')
+    session[:user_id] = User.find_by_username(@access_token.params[:screen_name]).id
   end
   
 end
